1d25e54c40ed8660a4a66d2af43f8f7a996fdd75
AshwinGajbhiye <ashwingajbhiye36@gmail.com>
2023-10-22 23:02:01 +0530

    this issue is solved

--- Students/LCB2023023/Solution/dicenum.c
+++ Students/LCB2023023/Solution/dicenum.c
@@ -4,8 +4,10 @@
 
 int main(){
 int x,y;
+int t=0;
 int ali[] = {1,2,3,4,5,6};
 int bob[] = {1,2,3,4,5,6};
+while(t<3){
 printf("Enter score of alice between 1 to 6 : ");
 scanf("%d %d %d",&ali[0],&ali[1],&ali[2]);
 
@@ -16,17 +18,17 @@ x = ali[0]+ali[1]+ali[2];
 y = bob[0]+bob[1]+bob[2];
 
 if (x>y){
-    printf("alice");
+    printf("alice\n");
 }
 else if(x<y){
-    printf("bob");
+    printf("bob\n");
 }
 else{
-    printf("tie");
+    printf("tie\n");
+  }
+
+t++;
 }
-
-
-
     return 0;
 }
 
