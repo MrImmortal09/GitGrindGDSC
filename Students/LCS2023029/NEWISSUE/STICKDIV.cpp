87178f0fa91a5630c67f1d6d411c6684d497ac62
ArshiaAdhya <arshia82385@gmail.com>
2023-12-17 22:18:27 +0530

    fixed an error

--- Students/LCS2023029/NEWISSUE/STICKDIV.cpp
+++ Students/LCS2023029/NEWISSUE/STICKDIV.cpp
@@ -4,12 +4,18 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    int m = n/2;
-    int count = 0;
-    for (int i=1 ; i<m ; i++){
-        if ( i != (m-i)){
-            count++;
+    if (n%2 == 0){
+        int m = n/2;
+        int count = 0;
+        for (int i=1 ; i<=m ; i++){
+            if ( i != (m-i)){
+                count++;
+            }
         }
+        cout<<count/2;
     }
-    cout<<count/2;
+    else{
+        cout<<0;
+    }
+    return 0;
 }
